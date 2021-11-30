336e481052d8c73bc20cb5fd9d34ed83c9936d47
Alan Liu <ryutenu.tokyo@gmail.com>
2021-11-30 18:01:11 +0900

    [Mod] Storyboard

--- UICollectionView-Demo/ViewController.swift
+++ UICollectionView-Demo/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleCollectionView: UICollectionView!
+    
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        print(sender.tag)
+    }
 }
-
